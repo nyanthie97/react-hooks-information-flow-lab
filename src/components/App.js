47c6e28a04a44296db33d6d8e58cb41209620050
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-07-19 16:43:33 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 import Header from "./Header"
+import Filter from "./Filter";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
